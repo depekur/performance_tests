bbdf6a8c8077b0a9da513d2ccd00610d9662db7c
depekour <depekur@gmail.com>
2016-09-14 03:22:09 -0400

    +

--- 5.php
+++ 5.php
@@ -79,5 +79,70 @@
 		document.addEventListener("DOMContentLoaded", printFiles);		
 	</script>
 
+		<script>
+		function printFiles() {
+			var js = true,
+				 css = true;
+
+			var jsFiles = [
+				'https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js',
+				'http://cdn.jsdelivr.net/theaterjs/latest/theater.min.js',
+				'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/js/materialize.min.js',
+				'js/three.min.js',
+				'js/lazyLoad.js'
+			]; 
+
+			var cssLinks = [
+				'css/parts/reset.css',
+				'css/min/style_2.min.css',
+				'css/parts/laptop.css',
+				'css/parts/mobile.css',
+				'css/parts/slick.css',
+				'css/parts/tablet.css',
+				'css/parts/retina.css',
+				'css/parts/comments.css',
+				'css/parts/sidebar.css',
+			];
+
+			var result = '';
+			var jsFilesCount = jsFiles.length;
+
+			if (css) {
+				for (var i = 0; i < cssLinks.length; i++) {
+					result += '<link href="' + cssLinks[i] + '" rel="stylesheet">';
+				}
+				document.body.insertAdjacentHTML('beforeend', result);
+			} 
+
+			
+
+			if (js) {
+				print(jsFiles, jsFilesCount);
+			}
+
+			function print(jsFiles, jsFilesCount) {
+
+				if (jsFilesCount > 0) {
+					var fileId = jsFiles.length - jsFilesCount;
+					jsFilesCount--;
+
+					var script = document.createElement('script');
+					script.src = jsFiles[fileId];
+
+					script.onload = function() {
+						print(jsFiles, jsFilesCount);
+						if (script.src == 'js/lazyLoad.js') {
+							lazyImg();
+						}
+					}
+
+					document.body.appendChild(script);
+				}
+			}
+		}
+
+		document.addEventListener("DOMContentLoaded", printFiles);		
+	</script>
+
 </body>
 </html>
